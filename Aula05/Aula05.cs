ce02396845dc33131bd8d16967d3b926bd7cd9f2
NunoClodic Carvalho Lima <nunoinformaticaipg@gmail.com>
2025-02-23 00:01:19 +0000

    Aula06

---
+++ Aula05/Aula05.cs
@@ -0,0 +1,43 @@
+using System;
+
+class Aula05
+{
+    static void Main()
+    {
+        int resultado = (10 + 5);
+        int incremento = 10;
+
+        bool resultadoBool = (10 > 5);
+        incremento+=1;
+        // | or 
+        // & and 
+        // ^ xor
+        // ! not
+        // << deslocamento para a esquerda
+        // >> deslocamento para a direita
+        // == igual
+        // != diferente
+        // >= maior ou igual
+        // <= menor ou igual
+        // > maior
+        // < menor
+        // && and
+        // || or
+        // += incremento
+        // -= decremento
+        // *= multiplicação
+        // /= divisão
+        // %= resto
+        // ++ incremento
+        // -- decremento
+
+
+
+        Console.WriteLine("O resultado é: {0}", resultado);
+        Console.WriteLine("O resultado é: {0}", resultadoBool);
+        Console.WriteLine("O resultado Incremental é: {0}", incremento);
+    }
+
+}
+
+
---
+++ Aula06/Aula06.cs
@@ -0,0 +1,20 @@
+using System;
+
+static class Program {
+    static void Main() {
+
+        double valorCompra = 5.50;
+        double lucro = 0.35;
+        string produto = "Pastel de nata";
+        double valorVenda;
+
+        valorVenda = valorCompra + (valorCompra * lucro);
+
+        Console.WriteLine("\nProduto:............:{0,15}", produto);
+        Console.WriteLine("\n");
+        Console.WriteLine("Valor de Compra.....:{0,15:c}", valorCompra);
+        Console.WriteLine("Lucro...............:{0,15:p}", lucro);
+        Console.WriteLine("Valor de Venda......:{0,15:c}", valorVenda);
+
+    }
+}
