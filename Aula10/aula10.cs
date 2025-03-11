aadfab6037bb886a4ae4b6ebaabd3742205ae9ac
NunoClodic Carvalho Lima <nunoinformaticaipg@gmail.com>
2025-03-11 22:22:20 +0000

    Enumerações C#

---
+++ Aula10/aula10.cs
@@ -0,0 +1,20 @@
+using System;
+class Aula10// consceitos de enumerações e constantes em C# 
+{
+    enum DiasSemana{Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado};
+
+    static void Main()
+    {
+
+        DiasSemana ds = DiasSemana.Domingo;
+        DiasSemana ds2 = (DiasSemana)3;
+        int ds3 = (int)DiasSemana.Sexta;
+
+
+        Console.WriteLine(ds);
+        Console.WriteLine(ds2);
+        Console.WriteLine(ds3);
+
+
+    }
+}
