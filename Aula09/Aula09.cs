69fc07092e1955c67e98968ba579e0c86c0e71cc
NunoClodic Carvalho Lima <nunoinformaticaipg@gmail.com>
2025-03-11 22:09:46 +0000

    aula 9 bitwaise

---
+++ Aula09/Aula09.cs
@@ -0,0 +1,10 @@
+using System;
+class Aula09//conceitos de bitwaise operadores de deslocamento.
+{
+    static void Main()
+    {
+        int num=20;
+        num=num<<2; // desloca 1 bit para a direita
+        Console.WriteLine("Num: {0}", num);
+    }
+} 
