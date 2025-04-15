eeebf7d08320d518f614b71b080b897b588e6de8
NunoClodic Carvalho Lima <nunoinformaticaipg@gmail.com>
2025-04-15 23:49:42 +0100

    Out
    Argumento Out

---
+++ Aula26_ArgOut/aula26.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Aula24
+{ 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Qual valor do dividendo? ");
+        int dividendo = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Qual valor do divisor? ");
+        int divisor = Convert.ToInt32(Console.ReadLine());
+        int quociente;
+        int resto;
+        quociente = divide(dividendo, divisor, out resto); 
+
+        Console.WriteLine("*******************************************");
+        Console.WriteLine("{0}/{1} ",dividendo, divisor);
+        Console.WriteLine("Quociente: " + quociente);
+        Console.WriteLine("Resto: " + resto);
+
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+
+
+    }
+    static int divide(int dividendo, int divisor, out int resto)
+    {
+        int quociente = dividendo / divisor;
+        resto = dividendo % divisor;
+        return quociente;
+
+    }
+
+}
